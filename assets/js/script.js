82f84810ec771752475ef6e417c7e13920837ec8
PablitoGNU <Pablito.Dublino@gmail.com>
2019-06-04 18:00:01 +0000

    map added

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 6,
+            center: {
+            lat: 51.9194,
+            lng: 19.1451,
+}
+
+});
+
+}
