082b9abebd3ca88abba2fbfeedfbc15e75690f6a
PablitoGNU <Pablito.Dublino@gmail.com>
2019-06-09 21:21:22 +0000

    css corrected, image ofr footer removed, js added for maps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
-function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 6,
-            center: {
-            lat: 51.9194,
-            lng: 19.1451,
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    mapTypeControl: false,
+    panControl: false,
+    zoomControl: true,
+    streetViewControl: false,
+    center: {lat: 51.9194,lng: 19.1451,}
+});
+    infoWindow = new google.maps.InfoWindow({
+      content: document.getElementById('info-content')
+    });
+
+
+    var poland = {
+    types: ['(cities)'],
+    componentRestrictions: {country: "pl"}
+ 
+};
+
+    var input = document.getElementById('autocomplete');
+    autocomplete = new google.maps.places.Autocomplete(input, poland);
+
+    autocomplete.addListener('place_changed', onPlaceChanged);
 }
 
-});
-
+    function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+    if (place.geometry) {
+    map.panTo(place.geometry.location);
+    map.setZoom(12);
+    search();
+  }
 }
