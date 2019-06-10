71c7808218c929caa13aa5532790bbf5f4c6f791
PablitoGNU <Pablito.Dublino@gmail.com>
2019-06-10 10:46:15 +0000

    readme updated, js/html modifed

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 ## Visit Poland
 
-##### This is website will give users ability to search for their holiday destination in Poland. Users will be able to choose destination city, search flights , Find tourist attractions, Find accommodation, Find bars and restaurants. #####
+##### This is my offical website to promote Poland as a touristics Destination. #####
 
 #### Visit Poland ####
 
 ### UX
 
+*This website will give ability to explore Poland*
 
+*This is single page website* 
+*Home Page* White/Red color is placed on nice dark background.These two colors are defined in the Polish constitution as the national colors.
+ Website contains a bit of history of Poland,places to see and popular dishes. Addtinally users have ability to search for their holiday destination in Poland. 
+ Users will be able to choose destination city, Find tourist attractions, Find accommodation, Find bars and restaurants.
+*Nav links* will move users thorugh the page, *color* is changing on them from white to red.
+*CITY* will move users down where they can search for their holiday destination and Find tourist attractions, Find accommodation, Find bars and restaurants.
+*FLIGHTS* This link to skyscanner website, where users can search for flights. It will open in the new tab.
+*VIDEO* Embedded youtube video, This is animated history of Poland, by watching his short video users will be able to discover reach history of Poland. 
+*PICTURES* 
 ### Technologies Used
 
     1. HTML,
     2. CSS,
     3. BOOTSTRAP,
-    4. JS.
+    4. JS,
+    5. Google Maps Javascript API.
     
 ### Technical information and Testing
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-function initMap(){
+      var map, places;
+      var infoWindow;
+      var countryRestrict = {'country': 'pl'};
+      var autocomplete;
+      var markers = [];
+
+function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
     mapTypeControl: false,
@@ -7,28 +13,36 @@ function initMap(){
     streetViewControl: false,
     center: {lat: 51.9194,lng: 19.1451,}
 });
+    
     infoWindow = new google.maps.InfoWindow({
       content: document.getElementById('info-content')
-    });
+});
 
 
-    var poland = {
-    types: ['(cities)'],
-    componentRestrictions: {country: "pl"}
+autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */ (
+                document.getElementById('city')), {
+              types: ['(cities)'],
+              componentRestrictions: countryRestrict
+            });
+        places = new google.maps.places.PlacesService(map);
+
+        autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+// When the user selects a city, get the place details for the city and
+      // zoom the map in on the city.
+      function onPlaceChanged() {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+          map.panTo(place.geometry.location);
+          map.setZoom(11);
+          search();
+        } else {
+          document.getElementById('autocomplete').placeholder = 'Search...';
+}
+
+}          
+      
  
-};
-
-    var input = document.getElementById('autocomplete');
-    autocomplete = new google.maps.places.Autocomplete(input, poland);
-
-    autocomplete.addListener('place_changed', onPlaceChanged);
-}
-
-    function onPlaceChanged() {
-    var place = autocomplete.getPlace();
-    if (place.geometry) {
-    map.panTo(place.geometry.location);
-    map.setZoom(12);
-    search();
-  }
-}
+ 
